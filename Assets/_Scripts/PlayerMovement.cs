c41cc7d88ea150e06fc18754c7602caf46f3858c
1playerone1 <playerone1923@gmail.com>
2022-12-10 01:16:03 +0300

    added character movement

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,22 +3,50 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 moveInput;
+    [SerializeField] float runSpeed = 8f;
+    [SerializeField] float fallSpeed = 1f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
     void Start()
     {
-        
+        myRigidbody= GetComponent<Rigidbody2D>();
+        myAnimator= GetComponent<Animator>();
     }
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y * fallSpeed);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed); 
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
     }
 }
