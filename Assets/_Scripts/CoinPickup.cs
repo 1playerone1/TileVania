ff077e3792fca6349dd6bea0cb96acfe521a9141
1playerone1 <playerone1923@gmail.com>
2022-12-12 18:14:50 +0300

    made Hud

--- Assets/_Scripts/CoinPickup.cs
+++ Assets/_Scripts/CoinPickup.cs
@@ -6,11 +6,16 @@ public class CoinPickup : MonoBehaviour
 {
 
     [SerializeField] AudioClip coinSFX;
+    [SerializeField] int pointsForCoinPickups = 10;
+
+    bool wasCollected = false;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if(collision.tag == "Player" && !wasCollected)
         {
+            wasCollected = true;
+            FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickups);
             AudioSource.PlayClipAtPoint(coinSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
--- Assets/_Scripts/GameSession.cs
+++ Assets/_Scripts/GameSession.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,10 @@ public class GameSession : MonoBehaviour
 {
 
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Awake()
@@ -23,6 +28,12 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
     public void ProcessPlayerDeath()
     {
         if (playerLives > 1)
@@ -35,11 +46,18 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
     void TakeLife()
     {
         playerLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
     }
 
     void ResetGameSession()
